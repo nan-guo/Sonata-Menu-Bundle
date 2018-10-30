4a3347d63792f7477f523475fb20845d06d2bd30
Joseph Lemoine <lemoine.joseph@gmail.com>
2018-10-30 22:18:50 +0000

    Fix translation key and factory

--- DependencyInjection/ProdigiousSonataMenuExtension.php
+++ DependencyInjection/ProdigiousSonataMenuExtension.php
@@ -22,6 +22,6 @@ class ProdigiousSonataMenuExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader->load('services.yaml');
     }
 }
