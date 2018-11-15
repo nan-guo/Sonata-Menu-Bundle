326ce36f0a1d391462653dfca4eb721a67a51c1a
Nan Guo <guonanhq@gmail.com>
2018-11-15 16:54:31 +0100

    add callback function

--- Resources/public/js/sonata_menu.js
+++ Resources/public/js/sonata_menu.js
@@ -1,16 +1,2 @@
 'use strict'
-
-jQuery(document).ready(function($) {
-	var menuDepth = jQuery('#menu-depth').val();
-	var list = null;
-	var results = [];
-	var items = jQuery('#items');
-
-	jQuery('#nestable').nestable({
-        maxDepth: menuDepth,
-    }).on('change', function(e){
-    	list   = e.length ? e : jQuery(e.target);
-    	results = JSON.stringify(list.nestable('serialize'));
-    	items.val(results);
-    });
-});
+jQuery(document).ready(function($){var list=null;var results=[];var items=jQuery('#items');jQuery('#nestable').nestable({callback:function(l,e){list=l.length?l:jQuery(l.target);results=JSON.stringify(list.nestable('serialize'));items.val(results)}})})
